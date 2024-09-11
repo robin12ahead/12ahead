4aed611d80938ffd39ae0e9a8055e0945f483cc3
robin12ahead <robin@12ahead.ch>
2024-09-11 11:32:08 +0200

    Update floating-placeholders.js

--- attributes/forms/floating-placeholders/floating-placeholders.js
+++ attributes/forms/floating-placeholders/floating-placeholders.js
@@ -4,45 +4,36 @@
 
 $('[data-floating-placeholders="true"]').find('input, select, textarea, .w-imput, .form_input').each(function () {
 
-  var placeholderClassName = 'form_placeholder';
-  var placeholderClass = '.' + placeholderClass;
+    var placeholderClassName = 'form_placeholder';
+    var placeholderClass = '.' + placeholderClassName;
 
-  // remove all test placeholders beforehand
-  $(this).parent().find(placeholderClass).remove();
+    // remove all test placeholders beforehand
+    $(this).parent().find(placeholderClass).remove();
 
-  // get the correct placeholder text
-  let placeholder_text = "";
-  if ($(this).is('input, textarea')) {
-    placeholder_text = $(this).attr('placeholder');
-  } else if ($(this).is('select')) {
-    placeholder_text = $(this).find("option:first-child").text();
-  }
+    // get the correct placeholder text
+    let placeholder_text = "";
+    if ($(this).is('input, textarea')) {
+        placeholder_text = $(this).attr('placeholder');
+    } else if ($(this).is('select')) {
+        placeholder_text = $(this).find("option:first-child").text();
+    }
 
-  // define placeholder html
-  let placeholder_html = '<label class="' + placeholderClassName + '">' + placeholder_text + '</label>';
+    // define placeholder html
+    let placeholder_html = '<label class="' + placeholderClassName + '">' + placeholder_text + '</label>';
 
-  // put placeholder before each input
-  $(this).before(placeholder_html);
+    // put placeholder before each input
+    $(this).before(placeholder_html);
 
-  // hide all placeholders
-  if ($(this).find('input').attr('type') === "date") {
-    $(placeholderClass).show();
-  } else {
+    // hide all placeholders
     $(placeholderClass).hide();
-  }
+    $('input[type="date"]').parent().find(placeholderClass).show();
 
-  // show each placeholder if input is filled
-  if ($(this).hasClass('input_date')) {
-    $(this).on('focus', function (e) {
-      $(this).parent().find(placeholderClass).show();
-    });
-  } else {
+    // show each placeholder if input is filled
     $(this).on('input', function (e) {
-      if ($(this).val()) {
-        $(this).parent().find(placeholderClass).show();
-      } else {
-        $(this).parent().find(placeholderClass).hide();
-      }
+        if ($(this).val()) {
+            $(this).parent().find(placeholderClass).show();
+        } else {
+            $(this).parent().find(placeholderClass).hide();
+        }
     });
-  }
 });
