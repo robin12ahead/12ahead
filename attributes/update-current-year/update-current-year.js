56e22694414958697b124877c85cc6a356e9454e
robin12ahead <robin@12ahead.ch>
2025-01-10 13:50:23 +0100

    Create update-current-year.js

---
+++ attributes/update-current-year/update-current-year.js
@@ -0,0 +1,12 @@
+$ = jQuery;
+
+// function to automatically update the current year in the copyright footer
+$(document).ready(function () {
+
+    // get the current year
+    var currentYear = (new Date).getFullYear();
+
+    // override text with the current yar
+    $('span.current-year').text(currentYear);
+
+});
